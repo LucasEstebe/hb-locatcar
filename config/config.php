9b2a2db66e2b2942a6e0380ac13d306a229d6992
Lucas <estebeweb@gmail.com>
2020-02-11 11:26:40 +0100

    configuration array set up

--- config/config.php
+++ config/config.php
@@ -1,9 +1,16 @@
 <?php
 
+
+$configuration = [
+    'db' => [
+        'dsn'  => 'mysql:host=localhost;dbname=hblocatcars',
+        'user' => 'root',
+        'pass' => null,
+        ]
+];
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$container = new \App\Service\ServiceContainer();
+$container = new \App\Service\ServiceContainer($configuration);
 
 require_once __DIR__ . '/routes.php';
-
-
--- src/service/ServiceContainer.php
+++ src/service/ServiceContainer.php
@@ -4,6 +4,14 @@ use Bramus\Router\Router;
 
 class ServiceContainer {
     private $router;
+    private $pdo;
+    private $configuration;
+
+
+    public function __construct(array $configuration)
+    {
+        $this->configuration = $configuration;
+    }
 
     /**
      * @return mixed
@@ -14,4 +22,17 @@ class ServiceContainer {
         }
         return $this->router;
     }
+
+    /**
+     * @return PDO
+     */
+    public function getPdo()
+    {
+        if ($this->pdo === null){
+            $this->pdo = new PDO($this->configuration['db']['dsn'],$this->configuration['db']['user'],$this->configuration['db']['pass']);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        return $this->pdo;
+    }
+
 }
